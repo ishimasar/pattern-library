b7857db509f227827460e35b9ccc0b0c0ccedf49
ishimasar <ishimasar1106@gmail.com>
2021-05-09 21:17:43 +0900

    moving-text effect調整版公開

---
+++ public/js/vendor/moving-text.js
@@ -0,0 +1,16 @@
+moveLetter();
+function moveLetter() {
+  var textWrapper = document.querySelector('#js-anime-target');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, '<span class="letter">$&</span>');
+
+  anime.timeline({loop: false})
+    .add({
+      targets: '#js-anime-target .letter',
+      translateX: [40,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutQuad",
+      duration: 1000,
+      delay: (el, i) => 500 + 30 * i
+  });
+}
