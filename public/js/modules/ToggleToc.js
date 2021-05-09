ef139a2f2e2b2c7dd74d81ab46a2fe53ea0624db
ishimasar <ishimasar1106@gmail.com>
2021-05-10 01:14:03 +0900

    tocのJSエラー解消

--- public/js/modules/ToggleToc.js
+++ public/js/modules/ToggleToc.js
@@ -2,11 +2,13 @@ export function toggleToc() {
   const detailsElement = document.querySelector('.toc-open');
   const summaryElement = document.querySelector('.toc-open > summary');
 
-  detailsElement.addEventListener("toggle", () => {
-    if (detailsElement.open) {
-      summaryElement.innerHTML = '<span>[ 閉じる ]</span>'
-    } else {
-      summaryElement.innerHTML = '<span>[ 開く ]</span>'
-    }
-  });
+  if (detailsElement !== null) {
+    detailsElement.addEventListener("toggle", () => {
+      if (detailsElement.open) {
+        summaryElement.innerHTML = '<span>[ 閉じる ]</span>'
+      } else {
+        summaryElement.innerHTML = '<span>[ 開く ]</span>'
+      }
+    });
+  }
 }
