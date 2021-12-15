9dbffed5f367169c105fbccfab652ff84c22ebc2
ishimasar <ishimasar1106@gmail.com>
2021-12-15 20:23:38 +0900

    more update2

--- dest/js/modules/RangeSlider.js
+++ dest/js/modules/RangeSlider.js
@@ -4,7 +4,6 @@ export class RangeSlider {
     this.volumeValue = document.getElementsByClassName('volume-value')
   }
   changeRangeNum() {
-    // 選択した際のイベント取得
     this.range.addEventListener('input', () => {
       this.volumeValue[0].textContent = this.range.value;
     });
